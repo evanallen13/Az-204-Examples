40c9fbe50292b71577cb3525ef05e1b05c46419c
Evan <evanallen13.gmail.com>
2021-01-16 09:42:45 -0800

    add Create Queue

---
+++ ServiceBusPract_CreateQueue/ServiceBusPract_CreateQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace ServiceBusPract_CreateQueue
+{
+    class Program
+    {
+        private static string _bus_connectionstring = Settings._bus_connectionstring;
+        private static string _queue_name = $"{Settings._queue_name}_Three";
+        static async Task Main(string[] args)
+        {
+            CreateQueue(_queue_name).Wait();
+            Console.WriteLine("Queue created");
+        }
+
+        static async Task CreateQueue(string p_queuename)
+        {
+            ManagementClient _client = new ManagementClient(_bus_connectionstring);
+
+            var _description = new QueueDescription(p_queuename)
+            {
+                EnableBatchedOperations = false,
+                LockDuration = TimeSpan.FromMinutes(2),
+                MaxDeliveryCount = 2,
+                DefaultMessageTimeToLive = TimeSpan.FromMinutes(2)
+            };
+            var queue = await _client.CreateQueueAsync(_description);
+        }
+    }
+}
